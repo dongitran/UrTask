8316f40bd6cf6df98f9dcc095ad717e27c041176
Dong Tran <thiendong.iuh@gmail.com>
2024-10-15 07:39:37 +0700

    update cronjob

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
+mod config;
+mod error;
+mod models;
+mod services;
+mod handlers;
+mod utils;
+
 use crate::config::Config;
 use crate::utils::scheduler::run_scheduler;
 use dotenv::dotenv;
 use teloxide::prelude::*;
 use crate::services::database;
 use std::sync::Arc;
-use tokio::select;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -14,24 +20,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mongodb_client = database::connect_to_mongodb(&config.mongodb_uri).await?;
     let bot = Bot::new(&config.telegram_bot_token);
 
-    let handler = Update::filter_message()
-        .branch(
-            dptree::entry()
-                .filter_command::<handlers::command::Command>()
-                .endpoint(handlers::command::answer),
-        );
+    let handler = Update::filter_message().branch(
+        dptree
+            ::entry()
+            .filter_command::<handlers::command::Command>()
+            .endpoint(handlers::command::answer)
+    );
 
-    let dispatcher = Dispatcher::builder(bot, handler)
+    let mut dispatcher = Dispatcher::builder(bot, handler)
         .dependencies(dptree::deps![mongodb_client])
         .enable_ctrlc_handler()
         .build();
 
-    let scheduler_handle = tokio::spawn(run_scheduler(config.clone()));
-    
-    select! {
-        _ = dispatcher.dispatch() => println!("Bot stopped"),
-        _ = scheduler_handle => println!("Scheduler stopped"),
-    }
+    let dispatcher_future = dispatcher.dispatch();
+    let scheduler_future = run_scheduler(config.clone());
+
+    tokio::join!(dispatcher_future, scheduler_future);
 
     Ok(())
 }
