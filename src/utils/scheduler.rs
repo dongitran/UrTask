b2dbe8724cb9895969d34137bd0e1a5d6557f28b
Dong Tran <thiendong.iuh@gmail.com>
2024-11-19 01:54:38 +0700

    update time scheduler

--- src/utils/scheduler.rs
+++ src/utils/scheduler.rs
@@ -12,7 +12,7 @@ pub async fn run_scheduler(config: Arc<Config>) -> Result<(), AppError> {
     let gmt7 = FixedOffset::east_opt(7 * 3600).expect("Invalid timezone");
 
     scheduler.add(
-        Job::new_async("0 35 2 * * 2-6", {
+        Job::new_async("0 10 3 * * 2-6", {
             let config = config.clone();
             move |_, _| {
                 let config = config.clone();
@@ -28,7 +28,7 @@ pub async fn run_scheduler(config: Arc<Config>) -> Result<(), AppError> {
     ).await?;
 
     scheduler.add(
-        Job::new_async("0 10 3 * * 2-6", {
+        Job::new_async("0 35 2 * * 2-6", {
             let config = config.clone();
             move |_, _| {
                 let config = config.clone();
